6d8acd9cb93be7f1765c3a21e7de8b4e51338c67
hungvo2811 <109934537+hungvo2811@users.noreply.github.com>
2022-08-17 10:20:28 +0700

    Update staff.ts

--- Training/src/DAO/staff.ts
+++ Training/src/DAO/staff.ts
@@ -1,57 +1,64 @@
-import { AppDataSource } from "../config/config";
-import { staff } from "../entity";
-import { ICreateStaff, IQueryStaff } from "../models";
-const staffReponsitory = AppDataSource.getRepository(staff);
+import { AppDataSource } from '../';
+import { Staff } from '../entity';
+import { ICreateStaff, IQueryStaff } from '../models';
+const staffReponsitory = AppDataSource.getRepository(Staff);
 
-export default class Staff {
-  static getStaffByEmail = async (email: string): Promise<staff | null> => {
-    const result = await staffReponsitory.findOne({
-      where: {
-        email: email,
-      },
-      select: {
-        id: true,
-        name: true,
-        email: true,
-        password: true,
-      },
-    });
-    return result;
-  };
-  static getStaffById = async (id: number): Promise<staff | null> => {
-    const result = await staffReponsitory.findOne({
-      where: {
-        id: id,
-      },
-    });
-    return result;
-  };
-  static createStaff = async (staff: ICreateStaff): Promise<staff | null> => {
-    return await staffReponsitory.save(staffReponsitory.create(staff));
-  };
+export const getStaffByEmail = async (email: string): Promise<Staff | null> => {
+  const result = await staffReponsitory.findOne({
+    where: {
+      email: email,
+    },
 
-  static saveStaff = async (
-    staff: ICreateStaff,
-    id: number
-  ): Promise<Staff | null> => {
-    const oldStaff = await this.getStaffById(id);
-    if (!oldStaff) {
-      return null;
-    }
-    const updateStaff = await staffReponsitory.save(
-      Object.assign(oldStaff, staff)
-    );
-    return updateStaff;
-  };
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      password: true,
+    },
+  });
 
-  static getStaffs = async (query: IQueryStaff): Promise<Staff[]> => {
-    const limit = query.limit ? Math.floor(query.limit) : 10;
-    const keyword = query.keyword ? "%" + query.keyword + "%" : "%%";
-    const page = query.page > 0 ? Math.floor(query.page - 1) * limit : 1;
-    const queryString = staffReponsitory.createQueryBuilder();
-    queryString
-      .where("Staff.name LIKE :name", { name: `%${keyword}%` })
-      .orWhere("Staff.email LIKE :email", { email: `%${keyword}%` });
-    return await queryString.limit(limit).offset(page).getMany();
-  };
-}
+  return result;
+};
+
+export const getStaff = async (id: number): Promise<Staff | null> => {
+  const result = await staffReponsitory.findOne({
+    where: {
+      id: id,
+    },
+  });
+  return result;
+};
+
+export const createStaff = async (
+  staff: ICreateStaff
+): Promise<Staff | null> => {
+  return await staffReponsitory.save(staffReponsitory.create(staff));
+};
+
+export const saveStaff = async (
+  staff: ICreateStaff,
+  id: number
+): Promise<Staff | null> => {
+  const oldStaff = await getStaff(id);
+
+  if (!oldStaff) {
+    return null;
+  }
+
+  const updateStaff = await staffReponsitory.save(
+    Object.assign(oldStaff, staff)
+  );
+
+  return updateStaff;
+};
+
+export const getStaffs = async (query: IQueryStaff): Promise<Staff[]> => {
+  const limit = query.limit ? Math.floor(query.limit) : 10;
+  const keyword = query.keyword ? '%' + query.keyword + '%' : '%%';
+  const page = query.page > 0 ? Math.floor(query.page - 1) * limit : 1;
+  const queryString = staffReponsitory.createQueryBuilder();
+  queryString
+    .where('Staff.name LIKE :name', { name: `%${keyword}%` })
+    .orWhere('Staff.email LIKE :email', { email: `%${keyword}%` });
+  return await queryString.limit(limit).offset(page).getMany();
+};
