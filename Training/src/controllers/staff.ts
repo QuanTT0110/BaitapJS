f3daa22477a9000eb9988c69146894f1f93c9b50
hungvo2811 <109934537+hungvo2811@users.noreply.github.com>
2022-08-17 10:38:37 +0700

    Update staff.ts

--- Training/src/controllers/staff.ts
+++ Training/src/controllers/staff.ts
@@ -1,8 +1,8 @@
 import {
-  createStaff,
+  createStafff,
   updateStaff,
   getStaffById,
-  getStaffs,
+  getStaffS,
   checkEmail,
   deleteStaff,
 } from "../services";
@@ -14,40 +14,50 @@ export default class Staff {
     if (!result) {
       return res.status(404).json("Not found");
     }
+
     return res.status(200).json({ msg: "Get Success", data: result });
   };
+
   static getStaffs = async (req: Request, res: Response) => {
     const query: IQueryStaff = req.query as any;
-    const result = await getStaffs(query);
+    const result = await getStaffS(query);
     if (!result) {
       return res.status(404).json({ msg: "Get Failed" });
     }
+
     return res.status(200).json({ msg: "Success", data: result });
   };
+
   static createStaff = async (req: Request, res: Response) => {
     const staff: ICreateStaff = req.body as ICreateStaff;
     if ((await checkEmail(staff.email)) === true) {
       return res.status(404).json({ msg: "Email Already Taken" });
     }
-    const result = await createStaff(staff);
+
+    const result = await createStafff(staff);
     if (!result) {
       return res.status(404).json({ msg: "Create Failed" });
     }
+
     return res.status(200).json({ msg: "Create Success", data: result });
   };
+
   static updateStaff = async (req: Request, res: Response) => {
     const staff: ICreateStaff = req.body as ICreateStaff;
     const result = await updateStaff(staff, req.params.id);
     if (!result) {
       return res.status(404).json("Update Failed");
     }
+
     return res.status(200).json({ msg: "Update Success", data: result });
   };
+
   static deleteStaff = async (req: Request, res: Response) => {
     const result = await deleteStaff(req.params.id);
     if (result !== true) {
       return res.status(404).json("Delete Failed");
     }
+
     return res.status(200).json({ msg: "Delete Success" });
   };
 }
