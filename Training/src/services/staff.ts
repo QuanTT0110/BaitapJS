30e65eab4ca872de391a364c007ed0a145a2e411
hungvo2811 <109934537+hungvo2811@users.noreply.github.com>
2022-08-17 10:35:49 +0700

    Update staff.ts

--- Training/src/services/staff.ts
+++ Training/src/services/staff.ts
@@ -1,54 +1,71 @@
-import { Staff } from "../DAO";
-import { ICreateStaff, IQueryStaff } from "../models";
+import {
+  getStaffByEmail,
+  getStaff,
+  getStaffs,
+  createStaff,
+  saveStaff,
+} from '../DAO';
+import { ICreateStaff, IQueryStaff } from '../models';
 
-export const createStaff = async (staff: ICreateStaff) => {
-  const result = await Staff.createStaff(staff);
+export const createStafff = async (staff: ICreateStaff) => {
+  const result: any = await createStaff(staff);
   return result;
 };
+
 export const updateStaff = async (staff: ICreateStaff, id: any) => {
-  const result = await Staff.saveStaff(staff, id);
+  const result = await saveStaff(staff, id);
   if (!result) {
     return null;
   }
+
   return result;
 };
+
 export const getStaffById = async (id: any) => {
-  const result = await Staff.getStaffById(id);
+  const result = await getStaff(id);
   if (!result) {
     return null;
   }
+
   return result;
 };
-export const getStaffs = async (query: IQueryStaff) => {
-  const result = await Staff.getStaffs(query);
+
+export const getStaffS = async (query: IQueryStaff) => {
+  const result = await getStaffs(query);
   if (!result) {
     return null;
   }
+
   return result;
 };
+
 export const deleteStaff = async (id: any) => {
-  const result = await Staff.getStaffById(id);
+  const result = await getStaff(id);
   if (!result) {
     return null;
   }
+
   await result.remove();
   return true;
 };
+
 export const loginStaffWithEmailAndPassword = async (
   email: string,
   password: string
 ) => {
-  const staff = await Staff.getStaffByEmail(email);
+  const staff = await getStaffByEmail(email);
   if (!staff || staff.password != password) {
     return false;
   }
+
   return staff;
 };
 
 export const checkEmail = async (email: string) => {
-  const oldStaff = await Staff.getStaffByEmail(email);
+  const oldStaff = await getStaffByEmail(email);
   if (oldStaff) {
     return true;
   }
+
   return false;
 };
