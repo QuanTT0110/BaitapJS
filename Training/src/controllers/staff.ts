8143de3a884ceeb186359b79ddebbde2684c8b00
hungvo2811 <109934537+hungvo2811@users.noreply.github.com>
2022-08-17 14:25:45 +0700

    Update staff.ts

--- Training/src/controllers/staff.ts
+++ Training/src/controllers/staff.ts
@@ -1,16 +1,11 @@
-import {
-  createStafff,
-  updateStaff,
-  getStaffById,
-  getStaffS,
-  checkEmail,
-  deleteStaff,
-} from "../services";
+import {services} from "../services";
 import { Request, Response } from "express";
 import { ICreateStaff, IQueryStaff } from "../models";
 export default class Staff {
   static getStaffById = async (req: Request, res: Response) => {
-    const result = await getStaffById(req.params.id);
+    const result = <Object|null>(
+      await services.getStaffById(req.params.id)
+    )
     if (!result) {
       return res.status(404).json("Not found");
     }
@@ -20,7 +15,10 @@ export default class Staff {
 
   static getStaffs = async (req: Request, res: Response) => {
     const query: IQueryStaff = req.query as any;
-    const result = await getStaffS(query);
+    const result = <Object|null> (
+      await services.getStaffs(query)
+    )
+
     if (!result) {
       return res.status(404).json({ msg: "Get Failed" });
     }
@@ -30,11 +28,15 @@ export default class Staff {
 
   static createStaff = async (req: Request, res: Response) => {
     const staff: ICreateStaff = req.body as ICreateStaff;
-    if ((await checkEmail(staff.email)) === true) {
+    
+    if ((await services.checkEmail(staff.email)) === true) {
       return res.status(404).json({ msg: "Email Already Taken" });
     }
 
-    const result = await createStafff(staff);
+    const result = <Object|null> (
+      await services.createStaff(staff)
+    )
+
     if (!result) {
       return res.status(404).json({ msg: "Create Failed" });
     }
@@ -44,7 +46,9 @@ export default class Staff {
 
   static updateStaff = async (req: Request, res: Response) => {
     const staff: ICreateStaff = req.body as ICreateStaff;
-    const result = await updateStaff(staff, req.params.id);
+    const result = <Object|null> (
+      await services.updateStaff(staff)
+    )
     if (!result) {
       return res.status(404).json("Update Failed");
     }
@@ -52,12 +56,4 @@ export default class Staff {
     return res.status(200).json({ msg: "Update Success", data: result });
   };
 
-  static deleteStaff = async (req: Request, res: Response) => {
-    const result = await deleteStaff(req.params.id);
-    if (result !== true) {
-      return res.status(404).json("Delete Failed");
-    }
-
-    return res.status(200).json({ msg: "Delete Success" });
-  };
 }
